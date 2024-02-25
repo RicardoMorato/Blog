c6ec4c5b3e7f67ea6c3ece3885f51d758d12b6f5
Ricardo Morato <rmr@cin.ufpe.br>
2024-02-25 00:36:21 -0300

    feat: export get_posts

--- frontend/js/store/reducers.js
+++ frontend/js/store/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
 import { restCheckReducer as restCheck } from "./rest_check";
+import { getPostsReducer as getPosts } from "./get_posts";
 
 export const rootReducer = combineReducers({
   restCheck,
+  getPosts,
 });
