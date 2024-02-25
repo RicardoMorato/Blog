b1ed09b48e98274b39342a2a12e8c339ec32feeb
Ricardo Morato <rmr@cin.ufpe.br>
2024-02-25 00:18:30 -0300

    feat: register posts routes

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -3,13 +3,16 @@ from django.urls import include, path
 
 import django_js_reverse.views
 from common.routes import routes as common_routes
+from posts.routes import routes as post_routes
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
 
-routes = common_routes
-for route in routes:
+for route in common_routes:
+    router.register(route["regex"], route["viewset"], basename=route["basename"])
+
+for route in post_routes:
     router.register(route["regex"], route["viewset"], basename=route["basename"])
 
 urlpatterns = [
